cbf0647d68b165f0efeae0187be8231adef5a8d3
Mike Metcalfe <mike@metcalfe.co.za>
2018-03-15 13:34:02 +0200

    Use ISO8601 for created_at json output

--- agent/utils.py
+++ agent/utils.py
@@ -7,10 +7,14 @@ def gen_unique_id():
     return datetime.now().strftime("%Y%m%d%H%M%S%f")
 
 
+def iso_8601(adate):
+    return adate.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
 class _JSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.date):
-            return obj.isoformat()
+            return iso_8601(obj)
         return super().default(obj)
     def iterencode(self, value):
         # Adapted from cherrypy/_cpcompat.py
