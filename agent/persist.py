b4577168d51849ea2b57a7f342db2a0d11b49314
Mike Metcalfe <mike@metcalfe.co.za>
2018-11-26 14:54:04 +0900

    Revert subject to a text field

--- agent/persist.py
+++ agent/persist.py
@@ -51,7 +51,7 @@ class Metadata(DocType):
                     {
                         "path_match": "metadata_json.subjects.subject",
                         "match_mapping_type": "string",
-                        "mapping": Keyword("not_analyzed")
+                        "mapping": Text(fields={'raw': Keyword()}),
                     }
             },
             {
